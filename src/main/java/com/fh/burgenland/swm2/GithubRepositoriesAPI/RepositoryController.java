42c1194179b567fca58d7d8465edeb96b2ff83af
basel-ui <105730412+basel-ui@users.noreply.github.com>
2023-01-28 10:22:54 +0100

    Added Request Mapping in RepositoryController

--- src/main/java/com/fh/burgenland/swm2/GithubRepositoriesAPI/RepositoryController.java
+++ src/main/java/com/fh/burgenland/swm2/GithubRepositoriesAPI/RepositoryController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-
+@RequestMapping("v1/github")
 public class RepositoryController {
     private final GitHubService gitHubService;
 
